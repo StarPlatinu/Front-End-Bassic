833a76b208a7d64fba728d019f29687404daae1d
ThanhVu <thanhvp1509@gmail.com>
2022-10-29 22:02:31 +0700

    btspr4 complete

--- BootstrapProject_04/1.js
+++ BootstrapProject_04/1.js
@@ -1,4 +1,12 @@
 ﻿ $(function(){
-	 
+    $('.navbar-nav li:nth-child(2) a').click(function(){
+           $('body').animate({scrollTop:750});
+           return false;
+     })
+
+     $('.navbar-nav li:nth-child(3) a').click(function(){
+        $('body').animate({scrollTop:1200});
+        return false;
+  })
 })  
  
