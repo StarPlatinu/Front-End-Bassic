e7d993475a45f2f4677617f89b8c06c81666839d
ThanhVu <thanhvp1509@gmail.com>
2022-10-31 14:46:00 +0700

    bstrpr5

---
+++ BootstrapProject_05/1.js
@@ -0,0 +1,11 @@
+﻿ $(function(){
+var docao = $(window).height();
+$('.top1').css({'height':docao})
+
+$(window).resize(function(){
+	var docao = $(window).height();
+	$('.top1').css({'height':docao})
+});
+
+})  
+ 
